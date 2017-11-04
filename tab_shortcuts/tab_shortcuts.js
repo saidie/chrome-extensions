315b1c887efdc61995d21a155a1d44f0a5844bf7
Hiroshi Saito <hiroshi.saito@smartnews.com>
2017-11-04 16:32:06 +0900

    Remove tab cycle shortcuts

--- tab_shortcuts/tab_shortcuts.js
+++ tab_shortcuts/tab_shortcuts.js
@@ -1,26 +1,6 @@
 chrome.commands.onCommand.addListener(function(command) {
   var callback;
-  if ( command === "show-next-tab" ) {
-    callback = function(tabs) {
-      var tab = tabs[0];
-      chrome.tabs.query({ windowId: tab.windowId, index: tab.index + 1 }, function(tabs) {
-        var tab = tabs[0];
-        chrome.tabs.update(tab.id, { active: true });
-      });
-    };
-  }
-  else if ( command === "show-prev-tab" ) {
-    callback = function(tabs) {
-      var tab = tabs[0];
-      if ( tab.index > 0 ) {
-        chrome.tabs.query({ windowId: tab.windowId, index: tab.index - 1 }, function(tabs) {
-          var tab = tabs[0];
-          chrome.tabs.update(tab.id, { active: true });
-        });
-      }
-    };
-  }
-  else if ( command === "move-tab-next" ) {
+  if ( command === "move-tab-next" ) {
     callback = function(tabs) {
       var tab = tabs[0];
       chrome.tabs.move(tab.id, { index: tab.index + 1 });
